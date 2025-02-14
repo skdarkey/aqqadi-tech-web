b59601840f5931226c10c1dad6df5ad4e9fb9cbc
Selorm Komla <92562695+skdarkey@users.noreply.github.com>
2025-02-14 15:42:11 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// script.js
+// document.querySelector(".card").addEventListener("click", function () {
+//     this.classList.toggle("flipped");
+// });
+
+// make all cards flip
+document.querySelectorAll(".card").forEach(card => {
+    card.addEventListener("click", function () {
+        this.classList.toggle("flipped");
+    });
+});
+
+// hambuger navbar
+document.getElementById("menu-toggle").addEventListener("click", function () {
+    document.getElementById("nav-links").classList.toggle("show");
+});
+
+
